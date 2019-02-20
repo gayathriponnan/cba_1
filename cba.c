e6ebc819d32e51d87b59271a9526517021acc383
gayathriponnan <36539737+gayathriponnan@users.noreply.github.com>
2019-02-20 22:24:22 +0530

    Update cba.c

--- cba.c
+++ cba.c
@@ -4,11 +4,11 @@ int main()
     int number;
 
     printf("Enter an integer: ");
-    scanf("%f", &number);
+    scanf("%d", &number);
 
     // True if the number is perfectly divisible by 2
     if(number % 2 == 0)
-        printf("%d is even/n", number);
+        printf("%d is even\n", number);
     else
         printf("%d is odd.", number);
 
